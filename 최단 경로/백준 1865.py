3ca69e915d9b823b17a85ee76dd776333751157e
Yegeun Kim <yegkim@c5r7s7.42seoul.kr>
2024-04-01 11:17:03 +0900

    240401

---
+++ 최단 경로/백준 1865.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+TC = int(input())
+
+INF = int(1e9)
+
+def bellman_ford(graph) :
+    v = len(graph)
+    dist[1] = 0
+    for i in range(1, v) :
+        for edge in graph :
+            if dist[edge[1]] > dist[edge[0]] + edge[2] :
+                dist[edge[1]] = dist[edge[0]] + edge[2]
+                if i == v - 1 :
+                    return True
+        # print(i, dist)
+    return False
+
+for t in range(TC) :
+    N, M, W = map(int, input().split())
+    graph = []
+    for m in range(M) :
+        S, E, T = map(int, input().split())
+        graph.append((S, E, T))
+        graph.append((E, S, T))
+    for w in range(W) :
+        S, E, T = map(int, input().split())
+        graph.append((S, E, -T))
+    flag = 0
+    dist = [INF] * (N + 1)
+    # print(graph)
+    if bellman_ford(graph) :
+        print("YES")
+    else :
+        print("NO")
+    # print("----------------")
+    # print(dist)
